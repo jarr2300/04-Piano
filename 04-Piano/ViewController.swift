3dbf962af2ed80e29845ea4a7d592fc03899aeb8
Macabuku <jarr2300foros@gmail.com>
2018-12-29 18:23:18 +0100

    Initial Commit

---
+++ 04-Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  04-Piano
+//
+//  Created by Macabuku on 29/12/18.
+//  Copyright © 2018 Macabuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
